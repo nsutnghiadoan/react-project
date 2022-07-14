6ca3b6bdce74f09b89298ed098f1eb6a7800c204
pgstsnghiadoan <nghiadoan2k@gmail.com>
2022-07-14 09:17:10 +0700

    add chakra ui to todolist

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,11 +3,24 @@ import '../assets/css/todo.scss'
 import TodoInput from './TodoInput';
 import TodoItem from './TodoItem';
 import TodoList from './TodoList';
+import {Container} from '@chakra-ui/react';
+import { Row, Col } from 'react-bootstrap';
+
 function Todo() {
    return (
     <div className='todo'>
-        <TodoInput />
-        <TodoList />
+        <Container>
+            <Row>
+                <Col>
+                    <TodoInput />
+                    <TodoList />
+                </Col>
+                <Col>
+                    {/* <Filter></Filter> */}
+                    <h1>àghjk</h1>
+                </Col>
+            </Row>
+        </Container>
     </div>
    )
 }
